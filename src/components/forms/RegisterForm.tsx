7113c534d2d8d84a475d43c5ea36b51052eb4ea4
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-10-26 19:54:52 +0500

    add new codes

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,10 +1,12 @@
-import { InputContainer, InputField } from "../../utils/styles";
+import { InputContainer, InputField, InputLabel } from "../../utils/styles";
+import styles from "./index.module.scss";
 
 export const RegisterForm = () => {
   return (
-    <form>
-      <InputContainer >
-      
+    <form className={styles.form}>
+      <InputContainer>
+        <InputLabel htmlFor="email">Email</InputLabel>
+        <InputField type="email" id="email" />
       </InputContainer>
     </form>
   );
--- src/utils/styles/index.tsx
+++ src/utils/styles/index.tsx
@@ -1,7 +1,27 @@
 import styled from "styled-components";
 
-export const InputField = styled.input``;
+export const InputField = styled.input`
+  font-family: "Inter";
+  outline: none;
+  border: none;
+  background-color: inherit;
+  color: #fff;
+  font-size: 18px;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 4px 0;
+`;
 
-export const InputContainer = styled.div``;
+export const InputContainer = styled.div`
+  background-color: #131313;
+  padding: 12px 16px;
+  border-radius: 10px;
+`;
 
-export const InputLabel = styled.label``;
+export const InputLabel = styled.label`
+  display: block;
+  color: #8f8f8f;
+  font-size: 14px;
+  margin: 4px 0;
+`;
